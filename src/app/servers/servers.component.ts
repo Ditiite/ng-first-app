aa775b56c44d24b79a3f2e5dd94c83d3acce0b4c
Dita <dita.rahmane.silova@inbox.lv>
2020-09-16 23:08:34 +0300

    Assignment 32.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -30,6 +30,9 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  show:boolean = true;
+  log = [];
 
   // Assignment 23.Practicing databinding
   username = '';
@@ -47,10 +50,19 @@ export class ServersComponent implements OnInit {
   
   onCreateServer() {
     this.serverCreated = true
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
+
+  toggle() {
+    this.show = !this.show;
+
+    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
 }
